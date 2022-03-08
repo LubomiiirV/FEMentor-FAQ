d4af9190faadc3ee5782a950cbedb17d46ea156f
Ľubomír Vaclavik <72025019+vlubosh@users.noreply.github.com>
2022-03-08 14:09:10 +0100

    click function
    added jquery click function - on click open element

---
+++ app.js
@@ -0,0 +1,19 @@
+$(".click").click(function () {
+    $(".show").toggleClass("unhide");
+  });
+
+  $(".click2").click(function () {
+    $(".show2").toggleClass("unhide");
+  });
+
+  $(".click3").click(function () {
+    $(".show3").toggleClass("unhide");
+  });
+
+  $(".click4").click(function () {
+    $(".show4").toggleClass("unhide");
+  });
+
+  $(".click5").click(function () {
+    $(".show5").toggleClass("unhide");
+  });
