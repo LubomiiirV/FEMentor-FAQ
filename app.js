5250128c07dcc7a213e6024a377d23ea804174f9
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-10 14:24:04 +0100

    changed function to slide toggle

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
-$(".click").click(function () {
-    $(".show").toggleClass("unhide");
-    $(".normal").toggleClass("bold");
-  });
+// $(".click").click(function () {
+//   $(".show").toggleClass("unhide");
+//   $(".normal").toggleClass("bold");
+// });
 
-  $(".click2").click(function () {
-    $(".show2").toggleClass("unhide");
-    $(".normal2").toggleClass("bold");
-  });
+// $(".click2").click(function () {
+//   $(".show2").toggleClass("unhide");
+//   $(".normal2").toggleClass("bold");
+// });
 
-  $(".click3").click(function () {
-    $(".show3").toggleClass("unhide");
-    $(".normal3").toggleClass("bold");
-  });
+// $(".click3").click(function () {
+//   $(".show3").toggleClass("unhide");
+//   $(".normal3").toggleClass("bold");
+// });
 
-  $(".click4").click(function () {
-    $(".show4").toggleClass("unhide");
-    $(".normal4").toggleClass("bold");
-  });
+// $(".click4").click(function () {
+//   $(".show4").toggleClass("unhide");
+//   $(".normal4").toggleClass("bold");
+// });
 
-  $(".click5").click(function () {
-    $(".show5").toggleClass("unhide");
-    $(".normal5").toggleClass("bold");
+// $(".click5").click(function () {
+//   $(".show5").toggleClass("unhide");
+//   $(".normal5").toggleClass("bold");
+// });
+
+//new method - slidetoggle
+$(document).ready(function(){
+  $('.hidden').hide();
+});
+
+$(document).ready(function(){
+  $(".click").click(function(){
+    $(".show").slideToggle("fast");
   });
+});
+
+$(document).ready(function(){
+  $(".click2").click(function(){
+    $(".show2").slideToggle("fast");
+  });
+});
+
+$(document).ready(function(){
+  $(".click3").click(function(){
+    $(".show3").slideToggle("fast");
+  });
+});
+
+$(document).ready(function(){
+  $(".click4").click(function(){
+    $(".show4").slideToggle("fast");
+  });
+});
+
+$(document).ready(function(){
+  $(".click5").click(function(){
+    $(".show5").slideToggle("fast");
+  });
+});
