601a7f0b7b908daa9e915aa8a593166681bc2d56
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-10 12:28:57 +0100

    added function for onclick h3 bold

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 $(".click").click(function () {
     $(".show").toggleClass("unhide");
+    $(".normal").toggleClass("bold");
   });
 
   $(".click2").click(function () {
     $(".show2").toggleClass("unhide");
+    $(".normal2").toggleClass("bold");
   });
 
   $(".click3").click(function () {
     $(".show3").toggleClass("unhide");
+    $(".normal3").toggleClass("bold");
   });
 
   $(".click4").click(function () {
     $(".show4").toggleClass("unhide");
+    $(".normal4").toggleClass("bold");
   });
 
   $(".click5").click(function () {
     $(".show5").toggleClass("unhide");
+    $(".normal5").toggleClass("bold");
   });
