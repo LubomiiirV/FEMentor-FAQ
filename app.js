5211f25d2d7f4a0a598ec642b48c5a8a3381c359
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-10 14:26:37 +0100

    forgot to add bold to h3 onclick

--- app.js
+++ app.js
@@ -31,29 +31,34 @@ $(document).ready(function(){
 $(document).ready(function(){
   $(".click").click(function(){
     $(".show").slideToggle("fast");
+    $(".normal").toggleClass("bold");
   });
 });
 
 $(document).ready(function(){
   $(".click2").click(function(){
     $(".show2").slideToggle("fast");
+    $(".normal2").toggleClass("bold");
   });
 });
 
 $(document).ready(function(){
   $(".click3").click(function(){
     $(".show3").slideToggle("fast");
+    $(".normal3").toggleClass("bold");
   });
 });
 
 $(document).ready(function(){
   $(".click4").click(function(){
     $(".show4").slideToggle("fast");
+    $(".normal4").toggleClass("bold");
   });
 });
 
 $(document).ready(function(){
   $(".click5").click(function(){
     $(".show5").slideToggle("fast");
+    $(".normal5").toggleClass("bold");s
   });
 });
