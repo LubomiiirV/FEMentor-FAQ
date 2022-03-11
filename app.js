2770aee4527ef7cae332780a031da72796cd3f0d
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-11 10:04:41 +0100

    added desktop view

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// $(".click").click(function () {
-//   $(".show").toggleClass("unhide");
-//   $(".normal").toggleClass("bold");
-// });
-
-// $(".click2").click(function () {
-//   $(".show2").toggleClass("unhide");
-//   $(".normal2").toggleClass("bold");
-// });
-
-// $(".click3").click(function () {
-//   $(".show3").toggleClass("unhide");
-//   $(".normal3").toggleClass("bold");
-// });
-
-// $(".click4").click(function () {
-//   $(".show4").toggleClass("unhide");
-//   $(".normal4").toggleClass("bold");
-// });
-
-// $(".click5").click(function () {
-//   $(".show5").toggleClass("unhide");
-//   $(".normal5").toggleClass("bold");
-// });
-
 //new method - slidetoggle
 $(document).ready(function(){
   $('.hidden').hide();
