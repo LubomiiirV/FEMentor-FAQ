c64eb0d0bdc0e0fc169022cc2654dd95ccbf48a1
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-14 13:41:38 +0100

    restyling

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//new method - slidetoggle
+//default visibility
 $(document).ready(function () {
   $(".hidden").hide();
 });
-//1
+
+//toggle first
 $(document).ready(function () {
   $(".click").click(function () {
     $(".show").slideToggle("fast");
@@ -10,48 +11,44 @@ $(document).ready(function () {
     if ($(".hidden").hasClass("show")) {
       $(".show2, .show3, .show4, .show5").hide();
       $(".normal2, .normal3, .normal4, .normal5").removeClass("bold");
-    } else {
     }
   });
 });
-//2
+
+//toggle second
 $(document).ready(function () {
   $(".click2").click(function () {
     $(".show2").slideToggle("fast");
     $(".normal2").toggleClass("bold");
     if ($(".hidden").hasClass("show")) {
-      $(".show, .show3,.show4,.show5").hide();
-      $(".normal,.normal3, .normal4, .normal5").removeClass("bold");
-    } else {
-      
+      $(".show, .show3, .show4, .show5").hide();
+      $(".normal, .normal3, .normal4, .normal5").removeClass("bold");
     }
   });
 });
-//3
+
+//toggle third
 $(document).ready(function () {
   $(".click3").click(function () {
     $(".show3").slideToggle("fast");
     $(".normal3").toggleClass("bold");
     $(".hidden").addClass("hideAgain");
     if ($(".hidden").hasClass("show")) {
-      $(".show, .show2,.show4,.show5").hide();
-      $(".normal,.normal2, .normal4, .normal5").removeClass("bold")
-    } else {
-      
+      $(".show, .show2, .show4, .show5").hide();
+      $(".normal, .normal2, .normal4, .normal5").removeClass("bold")
     }
   });
 });
-//4
+
+//toggle fourth
 $(document).ready(function () {
   $(".click4").click(function () {
     $(".show4").slideToggle("fast");
     $(".normal4").toggleClass("bold");
     $(".hidden").addClass("hideAgain");
     if ($(".hidden").hasClass("show")) {
-      $(".show, .show3,.show2,.show5").hide();
-      $(".normal,.normal3, .normal2, .normal5").removeClass("bold")
-    } else {
-      
+      $(".show, .show2, .show3, .show5").hide();
+      $(".normal, .normal2, .normal3, .normal5").removeClass("bold")
     }
   });
 });
@@ -62,10 +59,8 @@ $(document).ready(function () {
     $(".normal5").toggleClass("bold");
     $(".hidden").addClass("hideAgain");
     if ($(".hidden").hasClass("show")) {
-      $(".show, .show3,.show4,.show2").hide();
-      $(".normal,.normal3, .normal4, .normal2").removeClass("bold")
-    } else {
-      
+      $(".show, .show2, .show3, .show4 ").hide();
+      $(".normal, .normal2, .normal3, .normal4").removeClass("bold")
     }
   });
 });
