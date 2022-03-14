68cc6e83d959f56d4a97cde998454c7d5297ad1a
Lubomir Vaclavik <vaclaviklubomir@gmail.com>
2022-03-14 13:31:34 +0100

    finalized togle on click with hide other

--- app.js
+++ app.js
@@ -1,39 +1,71 @@
 //new method - slidetoggle
-$(document).ready(function(){
-  $('.hidden').hide();
+$(document).ready(function () {
+  $(".hidden").hide();
 });
-
-$(document).ready(function(){
-  $(".click").click(function(){
+//1
+$(document).ready(function () {
+  $(".click").click(function () {
     $(".show").slideToggle("fast");
     $(".normal").toggleClass("bold");
+    if ($(".hidden").hasClass("show")) {
+      $(".show2, .show3, .show4, .show5").hide();
+      $(".normal2, .normal3, .normal4, .normal5").removeClass("bold");
+    } else {
+    }
   });
 });
-
-$(document).ready(function(){
-  $(".click2").click(function(){
+//2
+$(document).ready(function () {
+  $(".click2").click(function () {
     $(".show2").slideToggle("fast");
     $(".normal2").toggleClass("bold");
+    if ($(".hidden").hasClass("show")) {
+      $(".show, .show3,.show4,.show5").hide();
+      $(".normal,.normal3, .normal4, .normal5").removeClass("bold");
+    } else {
+      
+    }
   });
 });
-
-$(document).ready(function(){
-  $(".click3").click(function(){
+//3
+$(document).ready(function () {
+  $(".click3").click(function () {
     $(".show3").slideToggle("fast");
     $(".normal3").toggleClass("bold");
+    $(".hidden").addClass("hideAgain");
+    if ($(".hidden").hasClass("show")) {
+      $(".show, .show2,.show4,.show5").hide();
+      $(".normal,.normal2, .normal4, .normal5").removeClass("bold")
+    } else {
+      
+    }
   });
 });
-
-$(document).ready(function(){
-  $(".click4").click(function(){
+//4
+$(document).ready(function () {
+  $(".click4").click(function () {
     $(".show4").slideToggle("fast");
     $(".normal4").toggleClass("bold");
+    $(".hidden").addClass("hideAgain");
+    if ($(".hidden").hasClass("show")) {
+      $(".show, .show3,.show2,.show5").hide();
+      $(".normal,.normal3, .normal2, .normal5").removeClass("bold")
+    } else {
+      
+    }
   });
 });
-
-$(document).ready(function(){
-  $(".click5").click(function(){
+//5
+$(document).ready(function () {
+  $(".click5").click(function () {
     $(".show5").slideToggle("fast");
-    $(".normal5").toggleClass("bold");s
+    $(".normal5").toggleClass("bold");
+    $(".hidden").addClass("hideAgain");
+    if ($(".hidden").hasClass("show")) {
+      $(".show, .show3,.show4,.show2").hide();
+      $(".normal,.normal3, .normal4, .normal2").removeClass("bold")
+    } else {
+      
+    }
   });
 });
